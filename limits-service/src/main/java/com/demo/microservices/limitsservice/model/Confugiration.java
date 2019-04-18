a12515486d28d9f493f0ad8e0bdfd80a13f12412
manish <manishgupta0690@gmail.com>
2019-04-18 23:10:18 +0530

    Update Confugiration.java

--- limits-service/src/main/java/com/demo/microservices/limitsservice/model/Confugiration.java
+++ limits-service/src/main/java/com/demo/microservices/limitsservice/model/Confugiration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 
+
 //author added
 @Component
 @ConfigurationProperties("limits-service")
